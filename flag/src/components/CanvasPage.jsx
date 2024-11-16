f53b5259e68453af5bc716ed340cbf7e555cc556
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-16 20:21:07 +0400

    add physics library and bus fall simulation

--- flag/src/components/CanvasPage.jsx
+++ flag/src/components/CanvasPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience'
 import Lights from './Lights'
+import { Physics } from '@react-three/rapier'
 
 const CanvasPage = () => {
   return (
@@ -10,8 +11,10 @@ const CanvasPage = () => {
         position: [5, 2, 5],
         fov: 50
      }}>
+        <Physics gravity={[0, -9.81, 0]}>
         <Lights />
         <Experience />
+        </Physics>
      </Canvas>
    </>
   )
--- flag/src/components/Experience.jsx
+++ flag/src/components/Experience.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 import { UserContext } from '../context/userContext';
+import { RigidBody } from '@react-three/rapier';
 
 const Experience = () => {
 
@@ -26,13 +27,15 @@ const Experience = () => {
   <>
  
  <OrbitControls />
-
+  <RigidBody type='fixed' >
   <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial color="#9EDF9C" />
       </mesh>
-
-      <primitive object={model.scene} scale = {0.6} position = {[0, -0.01, 0]} castShadow />
+      </RigidBody>
+   <RigidBody colliders = "hull" restitution={0.5} friction={0.8}>
+      <primitive object={model.scene} scale = {0.6} position = {[0, 10, 0]} castShadow />
+      </RigidBody>
   </>
   )
 }

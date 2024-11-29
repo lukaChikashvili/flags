bd800a70f05800c6fcfd135a02f37ccd34547312
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-29 17:03:01 +0400

    changed graident background color

--- flag/src/components/Experience.jsx
+++ flag/src/components/Experience.jsx
@@ -1,9 +1,8 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import { Environment, OrbitControls, useGLTF, useKeyboardControls } from '@react-three/drei'
 import * as THREE from 'three'
-import { UserContext } from '../context/userContext';
-import { RigidBody } from '@react-three/rapier';
-import { useFrame } from '@react-three/fiber';
+import vertex from '../shaders/background/vertex.glsl'
+import fragment from '../shaders/background/fragment.glsl'
 
 const Experience = () => {
 
@@ -13,7 +12,11 @@ let sphere = useRef(null);
 
 
 
-    
+    const uniforms = useRef({
+      color1: { value: new THREE.Color('#89A8B2')},
+      color2: { value: new THREE.Color('#B3C8CF')}
+    });
+
 
    
   
@@ -27,25 +30,32 @@ let sphere = useRef(null);
    <meshStandardMaterial color="red" />
  </mesh>
 
+
+
  <mesh receiveShadow position={[0, -0.5, 0]}>
-          <boxGeometry args={[10, 1, 50]} />
-          <meshStandardMaterial color="gray" />
-        </mesh>
+ 
+  <boxGeometry args={[20, 1, 100]} />
+  <shaderMaterial vertexShader={vertex} fragmentShader={fragment} uniforms={uniforms.current} />
+</mesh>
 
-        <mesh receiveShadow position={[-5.5, 2.5, 0]}>
-          <boxGeometry args={[1, 5, 50]} />
-          <meshStandardMaterial color="lightblue" />
-        </mesh>
+<mesh receiveShadow position={[-10.5, 5, 0]}>
 
-        <mesh receiveShadow position={[5.5, 2.5, 0]}>
-          <boxGeometry args={[1, 5, 50]} />
-          <meshStandardMaterial color="lightblue" />
-        </mesh>
+  <boxGeometry args={[1, 10, 100]} />
+  <shaderMaterial vertexShader={vertex} fragmentShader={fragment} uniforms={uniforms.current} />
+</mesh>
 
-        <mesh receiveShadow position={[0, 5.5, 0]}>
-          <boxGeometry args={[10, 1, 50]} />
-          <meshStandardMaterial color="white" />
-        </mesh>
+<mesh receiveShadow position={[10.5, 5, 0]}>
+
+  <boxGeometry args={[1, 10, 100]} />
+  <shaderMaterial vertexShader={vertex} fragmentShader={fragment} uniforms={uniforms.current} />
+</mesh>
+
+<mesh receiveShadow position={[0, 10.5, 0]}>
+
+  <boxGeometry args={[20, 1, 100]} />
+  <shaderMaterial vertexShader={vertex} fragmentShader={fragment} uniforms={uniforms.current} />
+</mesh>
+
 
 
  

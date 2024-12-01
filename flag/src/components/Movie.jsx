c23e4f93805e8cd3e88a19dbdecd75acd2ab892f
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-01 18:34:11 +0400

    about movie component design

--- flag/src/components/Movie.jsx
+++ flag/src/components/Movie.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Movie = ({ title, img }) => {
   return (
-    <div className=''>
+    <div className='flex items-center justify-center h-screen p-[20rem] gap-24'>
        <h1>{title}</h1>
-       <img src = {img} />
+       <img src = {img} className='w-[30rem] ' />
     </div>
   )
 }
